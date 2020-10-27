c66e81a9935f31f6bee5786d4c50c9f6a3698b98
Jonathan Turnock <Jonathan.Turnock@outlook.com>
2020-10-27 22:22:39 +0000

    Initial Commit

---
+++ docusaurus.config.js
@@ -0,0 +1,108 @@
+module.exports = {
+  title: 'Jonathan Turnock',
+  tagline: 'Software & Technology',
+  url: 'https://jonathanturnock.github.io/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'JonathanTurnock', // Usually your GitHub org/user name.
+  projectName: 'JonathanTurnock.github.io', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        // {
+        //   to: 'docs/',
+        //   activeBasePath: 'docs',
+        //   label: 'Docs',
+        //   position: 'left',
+        // },
+        {to: 'blog', label: 'Blog', position: 'right'},
+        {
+          href: 'https://github.com/JonathanTurnock',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Pet Projects',
+          items: [
+            {
+              label: 'Geoffrey',
+              to: 'https://bitbucket.org/fxqlabs-oss/geoffrey/src/master/',
+            },
+            {
+              label: 'Kodiak',
+              to: 'https://bitbucket.org/fxqlabs-oss/kodiak/src/master/',
+            },
+          ],
+        },
+        // {
+        //   title: 'Community',
+        //   items: [
+        //     {
+        //       label: 'Stack Overflow',
+        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //     },
+        //     {
+        //       label: 'Discord',
+        //       href: 'https://discordapp.com/invite/docusaurus',
+        //     },
+        //     {
+        //       label: 'Twitter',
+        //       href: 'https://twitter.com/docusaurus',
+        //     },
+        //   ],
+        // },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/JonathanTurnock',
+            },
+            {
+              label: 'BitBucket',
+              href: 'https://bitbucket.org/fxqlabs-oss/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Jonathan Turnock. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
